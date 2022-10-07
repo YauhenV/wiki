08aa09dd09a8c4927b86d512c61700a6d0913cb9
Eugene_Veryn <Gene9281@gmail.com>
2022-10-07 17:44:12 +0300

    Add React list on Names and Ages

--- Practice/react/name-age-list/src/App.js
+++ Practice/react/name-age-list/src/App.js
@@ -3,14 +3,19 @@ import NameList from './components/NameList/NameList';
 
 import React, { useState } from 'react';
 import './App.css';
+import PopUpInputValid from './components/pop-up/pop-up-input-valid';
 
 function App() {
+
+  let popUp;
 
   const [dataList, setDataList] = useState([
     { name: "Eugene", age: 30, id: "1" },
     { name: "Djony", age: 25, id: "2" },
     { name: "Jany", age: 18, id: "3" },
   ]);
+
+  const [isInputValid, setIsInputValid] = useState(true);
 
   const newDataSubmitHandler = (inputData) => {
     setDataList((prevData) => {
@@ -20,14 +25,38 @@ function App() {
     })
   }
 
+  const inputValidHandler = (inputValid) => {
+    setIsInputValid(inputValid);
+  }
+
+  const closePopUpHandler = (closePopUpFlag) => {
+    setIsInputValid(closePopUpFlag);
+  }
+
+  if (!isInputValid) {
+    popUp = <PopUpInputValid 
+      title={"Некорректный ввод"} 
+      text = {"Эти поля не могут быть пустыми!"} 
+      closePopUpFlag={closePopUpHandler}/>;
+  }
+
+  if (isInputValid === "lessOne") {
+    popUp = <PopUpInputValid 
+      title={"Некорректный возраст"} 
+      text = {"Возраст должен быть больше единицы!"} 
+      closePopUpFlag={closePopUpHandler}/>;
+   
+  }
+
   return (
     <div>
       <section className='name-input'>
-        <NameInput onSubmit={newDataSubmitHandler}/>
+        <NameInput onSubmit={newDataSubmitHandler} inputValid={inputValidHandler}/>
       </section>
       <section className='name-list'>
         <NameList dataList={dataList}/>
       </section>
+      {popUp}
     </div>
   )
 }
--- Practice/react/name-age-list/src/components/NameInput/NameInput.js
+++ Practice/react/name-age-list/src/components/NameInput/NameInput.js
@@ -4,23 +4,27 @@ import "./NameInput.css";
 const NameInput = (props) => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
-  const [isInputValid, setIsInputValid] = useState("false");
 
   const nameInputChangeHandler = (event) => {
-    if (event.target.value.trim().length < 1) {
-
-    }
-    console.log(event.target.value.trim().length)
-
     setName(event.target.value);
   }
 
   const ageInputChangeHandler = (event) => {
-    setAge(event.target.value);
+      setAge(event.target.value);
   }
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      props.inputValid(false);
+      return;
+    }
+    
+    if (age.trim() < 1) {
+      props.inputValid("lessOne");
+      return;
+      
+    }
     props.onSubmit([name, age]);
 
     setName("");
@@ -33,8 +37,8 @@ const NameInput = (props) => {
           <label className="input-form__input-title">Имя</label>  
           <input className="input-form__input" type="text" onChange={nameInputChangeHandler} value={name}></input> 
           <label className="input-form__input-title">Возраст</label>  
-          <input className="input-form__input" type="number" min="1" step="1" onChange={ageInputChangeHandler} value={age}></input> 
-          <button className="input-form__submit-button" type="submit">Добавить Пользователя</button>  
+          <input className="input-form__input" type="number" step="1" onChange={ageInputChangeHandler} value={age}></input> 
+          <button className="input-form__submit-button" type="submit">Добавить Пользователя</button> 
       </form>
     </div>
   );
